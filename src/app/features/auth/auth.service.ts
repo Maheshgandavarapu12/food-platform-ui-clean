ef8c4b3523a5b70fd0ad3955e567e5e0bdab76fe
Sai <saik55744@gmail.com>
2026-04-11 18:59:26 +0530

    dashboard

--- src/app/features/auth/auth.service.ts
+++ src/app/features/auth/auth.service.ts
@@ -1,15 +1,25 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { environment } from '../../shared/environments/environment';
-import { Observable, tap } from 'rxjs';
-import { LoginRequest } from '../../core/models/auth';
+import { Observable, of, tap } from 'rxjs';
+import { AccountStatus, AuthResponse, LoginRequest, User } from '../../core/models/auth';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private baseUrl = `${environment.apiUrl}/auth`;
-  constructor(private http: HttpClient) { }
+  private readonly TOKEN_KEY = 'access_token';
+  private readonly REFRESH_TOKEN_KEY = 'refresh_token';
+  private readonly USER_KEY = 'current_user';
+  private isBrowser: boolean;
+
+  constructor(private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { 
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   login(data: LoginRequest): Observable<any> {
     return this.http.post(`${this.baseUrl}/login`, data).pipe(
@@ -17,12 +27,46 @@ export class AuthService {
         if (!response || !response.accessToken) {
           throw new Error('Invalid credentials');
         }
-        localStorage.setItem('accessToken', response.accessToken);
+        this.setSessions(response);        
       })
     );
   }
 
+  logout(): Observable<any>{
+    localStorage.removeItem(this.TOKEN_KEY);
+    localStorage.removeItem(this.REFRESH_TOKEN_KEY);
+    localStorage.removeItem(this.USER_KEY);
+    return of(null)
+  }
+
   register(data: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/register`, data);
+  }
+
+  isLoggedIn(): boolean{
+    return !!this.getSession()
+
+  }
+
+  setSessions(response:AuthResponse){
+    if (!this.isBrowser) return;
+    const user: User = {
+      id: response.id,
+      firstName: response.firstName,
+      lastName: response.lastName,
+      email: response.email,
+      phoneNumber: response.phoneNumber,
+      roles: response.roles,
+      status: response.status || AccountStatus.Active,
+      emailVerified: response.emailVerified || false,
+      phoneVerified: response.phoneVerified || false,
+      profileImageUrl: response.profileImageUrl
+    };
+    localStorage.setItem(this.TOKEN_KEY, response.accessToken);
+    localStorage.setItem(this.USER_KEY, JSON.stringify(user));
+  }
+  getSession(): string | null{
+    if (!this.isBrowser) return null;
+    return localStorage.getItem(this.TOKEN_KEY);
   }
 }
--- src/app/shared/environments/environment.ts
+++ src/app/shared/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5104/api', // Your API URL
+  apiUrl: 'https://localhost:7018/api', // Your API URL
   appName: 'Food Platform',
   version: '1.0.0',
   tokenExpiryTime: 60, // minutes
